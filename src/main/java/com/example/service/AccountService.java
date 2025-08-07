8676848b0fb808501006a0974b3c522b834bac3c
Montre Jefferson <Jeffersonmontre@gmail.com>
2025-08-07 20:30:02 +0000

    Started initialization

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,16 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+    AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
 }
